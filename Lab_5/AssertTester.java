05a525f4a4a4ed10d2d9220704974731dd836f89
valen7zhi <val103@outlook.com>
2019-09-26 12:22:55 -0700

    lab 5 uploaded

---
+++ Lab_5/AssertTester.java
@@ -0,0 +1,11 @@
+// Name: Zhibo Wang
+// USC NetID: 7476147450
+// CSCI 455 Fall 2019
+// Lab 5
+
+public class AssertTester {
+    public static void main(String[] args) {
+        Term checking_1 = new Term(1.0, -1); // tester program intentionally makes Term.java assertion fail
+        Term checking_2 = new Term(1.0, 1); // valid input to Term.java
+    }
+}
---
+++ Lab_5/ReadTester.java
@@ -0,0 +1,44 @@
+// Name: Zhibo Wang
+// USC NetID: 7476147450
+// CSCI 455 Fall 2019
+// Lab 5
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReadTester {
+    public static void main(String[] args) {
+
+        String usrinput = new String();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a space separated list of numbers: ");
+        while (sc.hasNextLine()) {
+
+            usrinput = sc.nextLine().trim().replaceAll("\\s+", " "); // remove all white spaces via trim method and Java
+                                                                     // regular expression. ( +) in replaceAll method
+                                                                     // means to remove all spaces
+                                                                     // with one
+
+            if (usrinput.isEmpty()) {
+                System.out.println("usrinput " + "[]");
+                System.out.print("Enter a space separated list of numbers: ");
+                continue;
+            } else {
+                // System.out.println(usrinput);
+                // String[] usrinput2 = usrinput.trim().replaceAll(" +", " ").split(" ");
+                String[] usrinput2 = usrinput.split(" ");
+                // List<Integer> list = new ArrayList<Integer>();
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                for (int i = 0; i < usrinput2.length; i++) {
+                    list.add(Integer.parseInt(usrinput2[i]));
+                }
+                System.out.println("usrinput " + list);
+                System.out.print("Enter a space separated list of numbers: ");
+
+            }
+
+        }
+
+    }
+}
