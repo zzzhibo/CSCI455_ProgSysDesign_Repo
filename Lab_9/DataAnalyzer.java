c5f8829b97b8b8dfd7de90c7172706426f7cf97b
valen7zhi <val103@outlook.com>
2019-10-25 16:32:30 -0700

    lab9 updated

--- Lab_9/DataAnalyzer.java
+++ Lab_9/DataAnalyzer.java
@@ -3,41 +3,34 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
-   This program reads a file containing numbers and analyzes its contents.
-   If the file doesn't exist or contains strings that are not numbers, an
-   error message is displayed.
-*/
-public class DataAnalyzer
-{
-   public static void main(String[] args)
-   {
+ * This program reads a file containing numbers and analyzes its contents. If
+ * the file doesn't exist or contains strings that are not numbers, an error
+ * message is displayed.
+ */
+public class DataAnalyzer {
+   public static void main(String[] args) {
       Scanner in = new Scanner(System.in);
       DataSetReader reader = new DataSetReader();
-      
       boolean done = false;
-      while (!done) 
-      {
-         try 
-         {
-            System.out.println("Please enter the file name: ");
-            String filename = in.next();
-            
+
+      while (!done) {
+         System.out.println("Please enter the file name: ");
+         String filename = in.next();
+
+         try {
             double[] data = reader.readFile(filename);
             double sum = 0;
-            for (double d : data) { sum = sum + d; }
+            for (double d : data) {
+               sum = sum + d;
+            }
             System.out.println("The sum is " + sum);
             done = true;
-         }
-         catch (FileNotFoundException exception)
-         {
-            System.out.println("File not found.");
-         }
-         catch (BadDataException exception)
-         {
+         } catch (FileNotFoundException exception) {
+            System.out.println("File not found: " + filename);
+         } catch (BadDataException exception) {
             System.out.println("Bad data: " + exception.getMessage());
-         }
-         catch (IOException exception)
-         {
+            break;
+         } catch (IOException exception) {
             exception.printStackTrace();
          }
       }
--- Lab_9/DataSetReader.java
+++ Lab_9/DataSetReader.java
@@ -3,66 +3,63 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
-   Reads a data set from a file. The file must have the format
-   numberOfValues
-   value1
-   value2
-   . . .
-*/
-public class DataSetReader
-{
+ * Reads a data set from a file. The file must have the format numberOfValues
+ * value1 value2 . . .
+ */
+public class DataSetReader {
    private double[] data;
 
    /**
-      Reads a data set.
-      @param filename the name of the file holding the data
-      @return the data in the file
-   */
-   public double[] readFile(String filename) throws IOException
-   {
+    * Reads a data set.
+    * 
+    * @param filename the name of the file holding the data
+    * @return the data in the file
+    */
+   public double[] readFile(String filename) throws IOException {
       File inFile = new File(filename);
-      try (Scanner in = new Scanner(inFile))
-      {
+      try (Scanner in = new Scanner(inFile)) {
          readData(in);
          return data;
       }
    }
 
    /**
-      Reads all data.
-      @param in the scanner that scans the data
-   */
-   private void readData(Scanner in) throws BadDataException
-   {
-      if (!in.hasNextInt()) 
-      {
+    * Reads all data.
+    * 
+    * @param in the scanner that scans the data
+    */
+   private void readData(Scanner in) throws BadDataException {
+      if (!in.hasNextInt()) {
          throw new BadDataException("Length expected");
       }
       int numberOfValues = in.nextInt();
       data = new double[numberOfValues];
 
-      for (int i = 0; i < numberOfValues; i++)
-      {
+      for (int i = 0; i < numberOfValues; i++) {
          readValue(in, i);
       }
 
-      if (in.hasNext()) 
-      {
-         throw new BadDataException("End of file expected");
+      System.out.println(data.length);
+
+      if (in.hasNext()) {
+         throw new BadDataException("Too many data values given");
+         // throw new BadDataException("End of file expected");
       }
    }
 
    /**
-      Reads one data value.
-      @param in the scanner that scans the data
-      @param i the position of the value to read
-   */
-   private void readValue(Scanner in, int i) throws BadDataException
-   {
-      if (!in.hasNextDouble()) 
-      {
-         throw new BadDataException("Data value expected");
+    * Reads one data value.
+    * 
+    * @param in the scanner that scans the data
+    * @param i  the position of the value to read
+    */
+   private void readValue(Scanner in, int i) throws BadDataException {
+      if (!in.hasNextDouble()) {
+         throw new BadDataException("Non-floating point value give: " + in.next());
+         // throw new BadDataException("Data value expected");
+      } else if (!in.hasNext()) {
+         throw new BadDataException("Too few data values given");
       }
-      data[i] = in.nextDouble();      
+      data[i] = in.nextDouble();
    }
 }
